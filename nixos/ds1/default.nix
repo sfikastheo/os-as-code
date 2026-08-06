e99cda3990a77cf519350bcf42985b38674dbb5f
sfikastheo <theodore.sfikas@toolsforhumanity.com>
2026-08-06 18:48:57 +0200

    fix: momentarily disable firewall for ds1

--- nixos/ds1/default.nix
+++ nixos/ds1/default.nix
@@ -163,7 +163,10 @@
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+
+  # SSH + Netbird were broken, test without
+  # Firewall momentarily
+  networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
